874c1ac8bf93e0db22fe4708c720cf15c6b19e6f
Lucas Garcia <lucas.d.puerto@outlook.com>
2025-08-11 11:17:05 -0300

    Adiciona prints no script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+echo "Copiando configurações do Nginx..."
 cp nginx.conf /etc/nginx/nginx.conf
+
+echo "Copiando website para /var/wwww/html/"
 cp website/index.html /var/www/html/index.html
 
 nginx -s reload
+echo "Script executado com sucesso"
