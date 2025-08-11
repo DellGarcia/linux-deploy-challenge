c2c177e5ec3d0ebcb9da785b1c923ff9baa7a90a
Lucas Garcia <lucas.d.puerto@outlook.com>
2025-08-11 11:41:14 -0300

    Adicionando arquivos de estilo

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Copiando configurações do Nginx..."
 cp nginx.conf /etc/nginx/nginx.conf
 
 echo "Copiando website para /var/wwww/html/"
-cp website/index.html /var/www/html/index.html
+cp -r website/ /var/www/html/index.html
 
 nginx -s reload
 echo "Script executado com sucesso"
