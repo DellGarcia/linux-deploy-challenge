878cd9ec8d16d116ade75c3710c7610455e79fb9
Lucas Garcia <lucas.d.puerto@outlook.com>
2025-08-12 10:52:22 -0300

    Adiciona aspas

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -4,18 +4,18 @@ source $(dirname "$0")/.env.sh
 
 tryRestartServer()
 {
-    logToFile $LOG_ERROR_FILENAME "(Reiniciando)" "Tentando reiniciar o Servidor."
+    logToFile "$LOG_ERROR_FILENAME" "(Reiniciando)" "Tentando reiniciar o Servidor."
     systemctl restart nginx
 
     head=$( pingServer )
 
     if [ -z $head ]
         then
-            logToFile $LOG_ERROR_FILENAME "(Falha)" "Não foi possíveil reiniciar."
+            logToFile "$LOG_ERROR_FILENAME" "(Falha)" "Não foi possíveil reiniciar."
     elif [ $head -ne 200 ]
         then
-            logToFile $LOG_ERROR_FILENAME "(Status inesperado)" "Servidor retornou código $head"
+            logToFile "$LOG_ERROR_FILENAME" "(Status inesperado)" "Servidor retornou código $head"
     else      
-        logToFile $LOG_FILENAME "(Servidor Reiniciado)" "O servidor voltou a ativa"
+        logToFile "$LOG_FILENAME" "(Servidor Reiniciado)" "O servidor voltou a ativa"
     fi
 }
