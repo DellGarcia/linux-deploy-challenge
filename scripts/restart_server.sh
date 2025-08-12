42703f536d842b9f1e4c9abb6ab9c2b21fd8dfb5
Lucas Garcia <lucas.d.puerto@outlook.com>
2025-08-12 10:44:16 -0300

    Cria script de restart

---
+++ scripts/restart_server.sh
@@ -0,0 +1,21 @@
+source $(dirname "$0")/ping_server.sh
+source $(dirname "$0")/logger.sh
+source $(dirname "$0")/.env.sh
+
+tryRestartServer()
+{
+    logToFile $LOG_ERROR_FILENAME "Reiniciando" "Tentando reiniciar o Servidor."
+    systemctl restart nginx
+
+    head=$( pingServer )
+
+    if [ -z $head ]
+        then
+            logToFile $LOG_ERROR_FILENAME "(Falha)" "Não foi possíveil reiniciar."
+    elif [ $head -ne 200 ]
+        then
+            logToFile $LOG_ERROR_FILENAME "(Status inesperado)" "Servidor retornou código $head"
+    else      
+        logToFile $LOG_FILENAME "(Servidor Reiniciado)" "O servidor voltou a ativa"
+    fi
+}
